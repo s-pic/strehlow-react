bac777bf86221f549755e69840a99ac562c193b0
Sascha <sascha.picard@googlemail.com>
2019-06-14 14:01:17 +0200

    feat(Impressions): add gallery

--- src/pages/Impressions.js
+++ src/pages/Impressions.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 import Main from '~/components/MainSection';
+import { media } from '~/styles/Utils';
+import ImageGallery from '~/components/ImageGallery';
 
 const Wrapper = styled(Main)`
   display: block;
+  overflow: auto;
+  
+  ${media.s`
+    padding: 82px 12px 12px 12px;
+   `}
+  
+  ${media.l`
+     padding: 82px 48px 48px 48px;
+   `}
 `;
 
 const Impressions = () => (
-  <Wrapper />
+  <Wrapper>
+    <ImageGallery images={config.galleryImages.impressions} />
+  </Wrapper>
 );
-
-Impressions.propTypes = {
-};
-
-Impressions.defaultProps = {
-};
 
 export default Impressions;
