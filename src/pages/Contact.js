ed5eff7c6c5d6adc53f03cec2bef698cc970c535
Sascha <sascha.picard@googlemail.com>
2019-06-14 20:50:10 +0200

    style(Contact): use css-grid

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -13,54 +13,93 @@ const Wrapper = styled(Main)`
 const GridWrapper = styled.div`
   flex: 1;
   width: 100%;
-  max-width: 980px; 
   display: grid; 
-
-  grid-column-gap: 1em;
   justify-items: center;
   grid-template-columns: 100%; 
-
+  grid-template-rows: repeat(100px auto auto);
+  padding: 0 12px;
+  text-align: center;
+ 
   ${media.m`
+    max-width: 980px;   
     align-items: center;
     justify-items: center;
-    grid-template-rows: 200px 1fr 100px; 
-    grid-template-columns: 33% 33% 33%;
+    grid-template-rows: 200px auto 1fr 100px; 
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-column-gap: 48px;
+
+    footer {
+      grid-column: 1 / 4;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+    }
   `}
 `;
 
 const Heading = styled.h2`
-  margin: 2em 0;
+  margin: 0 0 1em 0;
+  ${media.m`
+    margin: 2em 0;
+  `}
 `;
 
 const Icon = styled.div`
   color: white;
-
+  width: 33%;
+  max-width: 100px;
+  align-self: center;
+ 
   ${media.m`
-    width: 50%;
-    height: 50%;
+    width: 33%;
+    height: 33%;
+    grid-row: 1;
   `}
 
-  width: 33%;
-  height: 33%;
-  
   svg {
     width: 100%;
     height: 100%;
   }
 `;
 
+const Topic = styled.h3`
+  margin: 1em 0;
+ 
+  ${media.m`
+    grid-row: 2;
+  `}
+`;
+
+const InfoText = styled.p`
+  align-self: flex-start;
+  margin-bottom: 3em;
+
+  ${media.m`
+    margin-bottom: 0;
+  `}
+`;
+
 const Contact = ({ heading }) => (
   <Wrapper>
     <Heading>{heading}</Heading>
     <GridWrapper>
+
       <Icon><MapPin /></Icon>
+      <Topic>Adresse</Topic>
+      <InfoText>Excepteur ut exercitation do tempor nostrud labore laborum excepteur irure est cillum excepteur esse. Consequat proident voluptate commodo voluptate. Qui ad exercitation adipisicing voluptate dolore elit.</InfoText>
+
       <Icon><Phone /></Icon>
+      <Topic>Telefon</Topic>
+      <InfoText>Excepteur ut exercitation do tempor nostrud labore laborum excepteur irure est cillum excepteur esse. Consequat proident voluptate commodo voluptate. Qui ad exercitation adipisicing voluptate dolore elit.</InfoText>
+
       <Icon><Mail /></Icon>
+      <Topic>E-Mail</Topic>
+      <InfoText>Excepteur ut exercitation do tempor nostrud labore laborum excepteur irure est cillum excepteur esse. Consequat proident voluptate commodo voluptate. Qui ad exercitation adipisicing voluptate dolore elit.</InfoText>
 
-      <p>Excepteur ut exercitation do tempor nostrud labore laborum excepteur irure est cillum excepteur esse. Consequat proident voluptate commodo voluptate. Qui ad exercitation adipisicing voluptate dolore elit.</p>
-      <p>Excepteur ut exercitation do tempor nostrud labore laborum excepteur irure est cillum excepteur esse. Consequat proident voluptate commodo voluptate. Qui ad exercitation adipisicing voluptate dolore elit.</p>
-      <p>Excepteur ut exercitation do tempor nostrud labore laborum excepteur irure est cillum excepteur esse. Consequat proident voluptate commodo voluptate. Qui ad exercitation adipisicing voluptate dolore elit.</p>
 
+      <footer>TODO: credits</footer>
     </GridWrapper>
   </Wrapper>
 );
