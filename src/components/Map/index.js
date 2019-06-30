0f0436eb7eb96c5944af1c62ee63af7b276c6320
Sascha <sascha.picard@googlemail.com>
2019-06-30 19:48:49 +0200

    style(map): smoothe zoom animation, closes #13

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -8,39 +8,40 @@ import bankImg from '~/images/icon/bank.jpg';
 import trainIcon from '~/images/icon/train_publicdomainvectors.png';
 import { getStrehlowMarkerPopupContent, getSeehausenMarkerPopupContent } from './PopupContents';
 
-export default () => {
-  const zoomToMarker = ({ map, position }) => map.easeTo({
-    zoom: map.getZoom() === 18 ? config.map.initialView.zoom : 18,
-    center: position
-  });
+const easing = t => t * (2 - t);
 
-  return (
-    <Map initialView={config.map.initialView} initialEaseZoom={config.map.initialEaseZoom}>
-      {map => (
-        <Fragment>
-          <LayerSwitcher map={map} />
-          <PopupMarker
-            map={map}
-            position={config.map.strehlowMarkerPosition}
-            onMarkerClick={() => zoomToMarker({ map, position: config.map.strehlowMarkerPosition })}
-            popupContent={getStrehlowMarkerPopupContent()}
-            markerImageSrc={bankImg}
-          />
-          <PopupMarker
-            map={map}
-            position={config.map.station.longLat}
-            onMarkerClick={() => zoomToMarker({ map, position: config.map.station.longLat })}
-            popupContent={getSeehausenMarkerPopupContent()}
-            markerImageSrc={trainIcon}
-            markerStyle={{ scale: 0.5, dropShadow: true, border: `1px solid ${config.colors.black}` }}
-          />
-          <GeoJsonOverlay
-            map={map}
-            geoJson={propertyShape}
-            layerId="strehlow-property"
-          />
-        </Fragment>
+const zoomToMarker = ({ map, position }) => map.flyTo({
+  zoom: map.getZoom() === 18 ? config.map.initialView.zoom : 18,
+  center: position,
+  easing
+});
+
+export default () => (
+  <Map initialView={config.map.initialView} initialEaseZoom={config.map.initialEaseZoom}>
+    {map => (
+      <Fragment>
+        <LayerSwitcher map={map} />
+        <PopupMarker
+          map={map}
+          position={config.map.strehlowMarkerPosition}
+          onMarkerClick={() => zoomToMarker({ map, position: config.map.strehlowMarkerPosition })}
+          popupContent={getStrehlowMarkerPopupContent()}
+          markerImageSrc={bankImg}
+        />
+        <PopupMarker
+          map={map}
+          position={config.map.station.longLat}
+          onMarkerClick={() => zoomToMarker({ map, position: config.map.station.longLat })}
+          popupContent={getSeehausenMarkerPopupContent()}
+          markerImageSrc={trainIcon}
+          markerStyle={{ scale: 0.5, dropShadow: true, border: `1px solid ${config.colors.black}` }}
+        />
+        <GeoJsonOverlay
+          map={map}
+          geoJson={propertyShape}
+          layerId="strehlow-property"
+        />
+      </Fragment>
       )}
-    </Map>
+  </Map>
   );
-};
